6321f60cfd3344d8562ce5c7ad4342a84ac586cb
Unknown <tomasz0zwierzynski@gmail.com>
2018-06-28 11:30:24 +0200

    Initial Commit, clean project with dependencies
    Web, jdbd, jpa, security, postgresql

---
+++ src/main/java/org/complex/SpringSecurityAngularPostgressRegisterApplication.java
@@ -0,0 +1,12 @@
+package org.complex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAngularPostgressRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAngularPostgressRegisterApplication.class, args);
+	}
+}
